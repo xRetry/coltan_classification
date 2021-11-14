Added KL-divergence; Fixed minor bugs;

classes/dataset.py
@@ -21,7 +21,11 @@ class Sample:
         self.sample_id = sample_id
         self.mine_id = mine_id
 
-    def __len__(self):
+    @property
+    def n_attributes(self) -> int:
+        return self.attributes.shape[1]
+
+    def __len__(self) -> int:
         return len(self.attributes)
 
 
@@ -104,6 +108,10 @@ class Dataset:
     @property
     def attributes(self) -> List[np.ndarray]:
         return [sample.attributes for sample in self._samples]
+
+    @property
+    def n_attributes(self) -> int:
+        return self._samples[0].n_attributes
 
     def __len__(self) -> int:
         return len(self._samples)

classes/mines.py
@@ -89,14 +89,12 @@ class BaselineMine(Mine):
 
 
 class BayesianSimpleMine(Mine):
-    def __init__(self, mean: np.ndarray, std: np.ndarray, **kwargs):
+    def __init__(self, mean: np.ndarray, cov: np.ndarray, **kwargs):
         super(BayesianSimpleMine, self).__init__(**kwargs)
-        mean = np.ones(37) * mean  # TODO: remove hardcoded prior-dim
-        cov = np.diag(np.ones(37) * np.power(std, 2))
         self._distribution = MultiNormal(mean, cov)
 
     def _add_sample(self, values) -> None:
-        cov_known = np.diag(np.ones(37))  # TODO: remove hardcoded prior-dim
+        cov_known = np.diag(np.ones(len(self.distribution)))
         self._distribution = self._distribution.posterior(self._func_transform(values), cov_known)
 
 

classes/models.py
@@ -33,11 +33,11 @@ class Model:
 
     def classify(self, sample: Sample) -> int:
         mines = list(self._mines.values())
-        eval_results, labels = [], []
-        for mine in mines:
-            eval_results.append(mine.eval_sample(sample))
-            labels.append(mine.status)
-        return self._parameters.func_selection(eval_results, mines)
+        eval_results, labels = np.zeros(len(mines)), np.zeros(len(mines))
+        for i, mine in enumerate(mines):
+            eval_results[i] = mine.eval_sample(sample)
+            labels[i] = mine.status
+        return self._parameters.func_selection(eval_results, labels)
 
     def cross_validate(self, dataset: Dataset, n_folds: int) -> float:
         samples = dataset.crossval_samples(n_folds, shuffle=True)