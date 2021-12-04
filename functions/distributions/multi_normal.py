6f8713623309e8b340b086766eb6e936908f0569
xRetry <marcoschwarz.at@gmail.com>
2021-12-04 11:42:36 +0100

    Added function descriptions

--- functions/distributions/multi_normal.py
+++ functions/distributions/multi_normal.py
@@ -1,39 +1,40 @@
 import numpy as np
-import scipy.stats
 from functions.decorators import verification
 
 
 @verification('a1', 'aa', 'a1')
 def pdf(mean: np.ndarray, cov: np.ndarray, x: np.ndarray) -> float:
+    """
+    Computes the pdf of a multivariate normal at the specified location.
+    """
     if not np.any(np.diag(cov) == 0):
         n = len(mean)
         det = np.linalg.det(cov)
         norm_const = 1 / (np.power(2 * np.pi, n / 2) * np.power(det, 1 / 2))
-        z = norm_const * np.exp(-1 / 2 * (x - mean).T @ np.linalg.inv(cov) @ (x - mean))  # TODO: Figure out why underflow occurs
-        return z[0][0]
+        p = norm_const * np.exp(-1 / 2 * (x - mean).T @ np.linalg.inv(cov) @ (x - mean))  # TODO: Figure out why underflow occurs
+        return p[0][0]
     return np.nan
 
 
 @verification('a1', 'aa', 'ba', 'aa')
 def posterior(mean: np.ndarray, cov:np.ndarray, x: np.ndarray, cov_known: np.ndarray) -> (np.ndarray, np.ndarray):
-    mean_prior = mean
-    cov_prior = cov
-    if len(x.shape) == 1:
-        n = 1
-        x = x[:, None]
-        mean_x = x
-    else:
-        n = len(x)
-        mean_x = x.mean(axis=0)[:, None]
+    """
+    Computes the parameters of the posterior distribution in case the true covariance matrix is known.
+    """
+    n = len(x)
+    mean_x = x.mean(axis=0)[:, None]
 
-    inverse = np.linalg.inv(cov_prior + (1 / n) * cov_known)
-    mu_post = cov_prior @ inverse * mean_x + (1 / n) * cov_known @ inverse @ mean_prior
-    cov_post = cov_prior @ inverse @ cov_known * (1 / n)
+    inverse = np.linalg.inv(cov + (1 / n) * cov_known)
+    mu_post = cov @ inverse * mean_x + (1 / n) * cov_known @ inverse @ mean
+    cov_post = cov @ inverse @ cov_known * (1 / n)
     return np.diag(mu_post)[:, None], cov_post
 
 
 @verification('a1', 'aa', 'a1', 'aa')
 def kl_divergence(mean1: np.ndarray, cov1: np.ndarray, mean2: np.ndarray, cov2: np.ndarray) -> float:
+    """
+    Computes the KL-Divergence between two multivariate normal distributions.
+    """
     # Removing zeros in diagonal of covariance matrix
     is_not_zero = (np.diag(cov1) != 0) & (np.diag(cov2) != 0)
     mean1 = mean1[is_not_zero, :]
