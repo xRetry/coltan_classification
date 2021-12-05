362668ecafe2ce75fc01af026e91365039d1e455
xRetry <marcoschwarz.at@gmail.com>
2021-12-05 14:04:26 +0100

    Added verification decorator

--- functions/estimators.py
+++ functions/estimators.py
@@ -1,14 +1,18 @@
 import numpy as np
+from functions.decorators import verification
 
 
+@verification('ba')
 def mean(attr_values: np.ndarray) -> np.ndarray:
     return np.mean(attr_values, axis=0)
 
 
+@verification('ba')
 def median(attr_values: np.ndarray) -> np.ndarray:
     return np.median(attr_values, axis=0)
 
 
+@verification('ba', None, None)
 def std(attr_values: np.ndarray, corrected=True, robust=False) -> np.ndarray:
     if robust:
         loc = median(attr_values)
@@ -21,6 +25,7 @@ def std(attr_values: np.ndarray, corrected=True, robust=False) -> np.ndarray:
     return np.sqrt(1 / n * np.sum(np.power((attr_values - loc), 2), axis=0))
 
 
+@verification('ba')
 def cov(attr_values: np.ndarray):
     return np.cov(attr_values.T)
 
