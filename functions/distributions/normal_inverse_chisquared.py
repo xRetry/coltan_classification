deba647b56525aff0d4084429c8a75a421671348
xRetry <marcoschwarz.at@gmail.com>
2021-12-05 12:32:18 +0100

    Added verification decorator; Fixed dimensionality errors in pdf functions

--- functions/distributions/normal_inverse_chisquared.py
+++ functions/distributions/normal_inverse_chisquared.py
@@ -1,30 +1,45 @@
 import numpy as np
 import scipy.special
 import scipy.stats
+from functions.decorators import verification
 
 
+@verification('a', 'a', '', '', 'ba')
 def posterior(mean: np.ndarray, std: np.ndarray, kappa: int, nu: int, x: np.ndarray) \
         -> (np.ndarray, np.ndarray, np.ndarray):
+    """
+        Computes the posterior parameters of a normal inverse chi-squared distribution given new samples.
+    """
+    # Pre-compute necessary values
     n = len(x)
-    x_mean = x.mean(axis=0)[:, None]
-    _var = np.power(std, 2)
-
+    x_mean = x.mean(axis=0)
+    var = np.power(std, 2)
+    # Computing the posterior parameters
     kappa_p = kappa + n
     mean_p = (kappa*mean + n*x_mean) / kappa_p
     nu_p = nu + n
-    std_p = np.sqrt(1/nu_p * (nu*_var + np.sum(np.power(x.T-x_mean, 2), axis=1)[:, None] + (n*kappa)/(kappa+n) * np.power(mean - x_mean, 2)))
+    std_p = np.sqrt(1/nu_p * (nu*var + np.sum(np.power(x - x_mean, 2), axis=0) + (n*kappa)/(kappa+n) * np.power(mean - x_mean, 2)))
     return mean_p, std_p, kappa_p, nu_p
 
 
+@verification('a', 'a', '', '', 'a', 'a')
 def pdf(mean, std, kappa: int, nu: int, x_mean: np.ndarray, x_std: np.ndarray) -> float:
-    _var = np.power(std, 2)
-    var = np.power(x_std, 2)
-    Z = np.sqrt(2*np.pi)/np.sqrt(kappa) * scipy.special.gamma(nu/2) * np.power(2/(nu * _var), nu/2)
-    return 1/Z * np.power(x_std, -1) * np.power(var, -(nu/2+1)) * np.exp(-1/(2*var) * (nu*_var + kappa*np.power(mean - x_mean, 2)))
+    """
+        Evaluates the pdf of a NIX distribution for a given mean and std.
+    """
+    var = np.power(std, 2)
+    x_var = np.power(x_std, 2)
+    Z = np.sqrt(2*np.pi)/np.sqrt(kappa) * scipy.special.gamma(nu/2) * np.power(2 / (nu * var), nu / 2)
+    p = 1 / Z * np.power(x_std, -1) * np.power(x_var, -(nu / 2 + 1)) * np.exp(-1 / (2 * x_var) * (nu * var + kappa * np.power(mean - x_mean, 2)))  # TODO: Fix underflow
+    return np.product(p)
 
 
-def pdf_predictive(mean: np.ndarray, std: np.ndarray, kappa: int, nu: int, x) -> float:
-    p = scipy.stats.t.pdf(  # TODO: Fix output dimensions
+@verification('a', 'a', '', '', 'a')
+def pdf_predictive(mean: np.ndarray, std: np.ndarray, kappa: int, nu: int, x: np.ndarray) -> float:
+    """
+        Evaluates the predictive pdf of a NIX distribution for new data.
+    """
+    p = scipy.stats.t.pdf(  # TODO: Fix underflow
         x=x,
         df=nu,
         loc=mean,
