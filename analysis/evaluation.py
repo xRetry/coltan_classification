cd977d372750d9fd8ca450fe6ea06a0337b2adae
xRetry <marcoschwarz.at@gmail.com>
2021-12-20 13:58:43 +0100

    Added method to evaluate functions alone; Removed depreciated functions

--- analysis/evaluation.py
+++ analysis/evaluation.py
@@ -5,63 +5,56 @@ from classes.parameters import Parameters
 from classes.dataset import Sample, Dataset
 from classes.normalizers import Normalization
 from classes.estimators import MeanUniEstimator
-from typing import List, Callable
+from typing import List, Callable, Optional, Tuple
 
 
 class EvalFuncAnalyser:
     @staticmethod
-    def func_analysis(eval_func: List[Callable] or Callable, x_min:float=-5, x_max:float=5,
-                      n_pts:int=100, normal_scale:float=1, n_samples:int=50):
-        self = EvalFuncAnalyser
-        if not isinstance(eval_func, list):
-            eval_func = [eval_func]
-
-        offsets_x = np.linspace(x_min, x_max, n_pts)
-        results_all = []
-        for func in eval_func:
-            np.random.seed(0)
-
-            attr_vals = np.random.normal(loc=1000, scale=normal_scale, size=n_samples)[:, None]
-            sample_mine = self._create_sample(attr_vals)
-            mine = self._create_mine(func)
-            mine.add_sample(sample_mine)
-
-            results_func = np.zeros_like(offsets_x)
-            for i, x_off in enumerate(offsets_x):
-                attr_vals_current = np.array(attr_vals)
-                attr_vals_current[:, 0] += x_off
-                sample_current = self._create_sample(attr_vals_current)
-                results_func[i] = mine.eval_sample(sample_current)
-
-            results_all.append(results_func)
-
-        labels = [func.__qualname__ for func in eval_func]
-        plotting.plot_eval_results(offsets_x, results_all, labels)
-
-    @staticmethod
-    def func_analysis_2d(eval_func: Callable, x_limits:tuple=(-5, 5), y_limits:tuple=(-5, 5)):
-        self = EvalFuncAnalyser
-        np.random.seed(0)
-
-        attr_vals = np.random.multivariate_normal(mean=[1000, 1000], cov=[[1, 0], [0, 1]], size=50)
-        sample_mine = self._create_sample(attr_vals)
-        mine = self._create_mine(eval_func)
-        mine.add_sample(sample_mine)
-
-        steps_x = np.linspace(*x_limits, 100)
-        steps_y = np.linspace(*y_limits, 100)
-
-        offsets_x, offsets_y = np.meshgrid(steps_x, steps_y)
-        results_eval = np.zeros_like(offsets_x)
-        for i in range(len(offsets_x)):
-            for j in range(len(offsets_y)):
-                attr_current = np.array(attr_vals)
-                attr_current[:, 0] += offsets_x[i, j]
-                attr_current[:, 1] += offsets_y[i, j]
-                sample_new = self._create_sample(attr_current)
-                results_eval[i, j] = mine.eval_sample(sample_new)
-
-        plotting.plot_eval_results_2d(offsets_x, offsets_y, results_eval)
+    def pure_analysis(func_eval: Callable or List[Callable or Tuple[Callable, dict]], x: np.ndarray=np.zeros(2),
+                      x1_range:Tuple[float]=(-3, 3), x2_range:Tuple[float]=(-3, 3)):
+        """
+        Shows the shape of the evaluation functions. Only works if the function takes single values.
+        """
+        # Wrap in list if single function
+        if not isinstance(func_eval, list):
+            func_eval = [func_eval]
+        # Setting up x value offsets
+        loc = np.array([0, 0])
+        x1_offset = np.linspace(*x1_range, 300)
+        x2_offset = np.linspace(*x2_range, 300)
+        x1_mesh, x2_mesh = np.meshgrid(x1_offset, x2_offset)
+        # Iterate through all functions
+        results = {}
+        for func in func_eval:
+            # Setting up function and addition argument
+            if isinstance(func, tuple):
+                kwargs = func[1]
+                func = func[0]
+            else:
+                kwargs = dict()
+            if not isinstance(kwargs, list):
+                kwargs = [kwargs]
+            # Iterate through function arguments
+            y_list = []
+            for kw in kwargs:
+                y = np.zeros_like(x1_mesh)
+                for i in range(x1_mesh.shape[0]):
+                    for j in range(x1_mesh.shape[1]):
+                        # Shift x values by offset
+                        x_shifted = np.array([
+                            x[0] + x1_mesh[i, j],
+                            x[1] + x2_mesh[i, j]
+                        ])
+                        # Evaluate function
+                        y[i, j] = func(loc, x_shifted, **kw)
+                y_list.append(y)
+            # Adding to result
+            results[func.__qualname__] = {
+                'y': y_list,
+                'kwargs': kwargs
+            }
+        # Plotting result
+        plotting.plot_eval_results(x1_mesh, x2_mesh, results)
 
     @staticmethod
     def _create_mine(eval_func: Callable) -> Mine:
@@ -91,32 +84,6 @@ class EvalFuncAnalyser:
             mine_id=''
         )
         return sample
-
-    @staticmethod
-    def sample_evaluation(eval_func:Callable):
-        self = EvalFuncAnalyser
-        dataset = Dataset()
-        mine_ids_unique, mine_ids_count = np.unique([smp.mine_id for smp in dataset], return_counts=True)
-        idx_selected = np.argmax(mine_ids_count)
-        samples_mine_selected = [sample for sample in dataset if sample.mine_id == mine_ids_unique[idx_selected]]
-        mine = self._create_mine(eval_func)
-
-        sample = samples_mine_selected[0]
-
-        mine.add_sample(sample)
-        multipliers = np.linspace(0.1, 1.9, 100)
-        dataset_analysis = dataset[:5]
-        eval_results = np.zeros((len(dataset_analysis), len(multipliers)))
-        x_vals = np.zeros_like(eval_results)
-        for s, sample in enumerate(dataset_analysis):
-            attr_orig = np.array(sample.attributes)
-            for m, multi in enumerate(multipliers):
-                sample.attributes = np.array(attr_orig)
-                sample.attributes[:, 0] *= multi
-                eval_results[s, m] = mine.eval_sample(sample)
-            x_vals[s, :] = attr_orig[:, 0].mean() * multipliers
-
-        plotting.plot_eval_results(x_vals, eval_results)
 
 
 if __name__ == '__main__':
