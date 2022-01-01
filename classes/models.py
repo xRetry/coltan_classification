7a413f3fe3597eadaeec9554e893c71212e5dd15
xRetry <marcoschwarz.at@gmail.com>
2022-01-01 23:10:31 +0100

    Changed Model to KernelModel; Changed Model to Interface; Created LogisticRegressionModel

--- classes/models.py
+++ classes/models.py
@@ -1,22 +1,35 @@
 import numpy as np
 import pandas as pd
+import abc
 from classes.mines import Mine
 from classes.parameters import Parameters
 from classes.dataset import Dataset, Sample
-from typing import List, Optional, Callable, Dict
+from typing import List, Optional, Callable, Dict, Iterable
+from sklearn.linear_model import LogisticRegression
 
 
-class Model:
+class Model(abc.ABC):
+    @staticmethod
+    def __init__(self, parameters: Parameters, samples: Iterable[Sample]):
+        pass
+
+    @staticmethod
+    def classify(self, sample: Sample) -> int:
+        pass
+
+
+class KernelModel(Model):
     _mines: Dict[str, Mine]
     _parameters: Parameters
 
-    def __init__(self,parameters: Parameters, dataset: Dataset):
+    def __init__(self, parameters: Parameters, samples: Iterable[Sample]):
+        super().__init__(self, parameters, samples)
         self._mines = {}
         self._parameters = parameters
-        self._create_mines(dataset)
+        self._create_mines(samples)
 
-    def _create_mines(self, dataset: Dataset) -> None:
-        for sample in dataset:
+    def _create_mines(self, samples: Iterable[Sample]) -> None:
+        for sample in samples:
             self._add_sample(sample)
 
     def _add_sample(self, sample: Sample):
@@ -38,6 +51,21 @@ class Model:
             eval_results[i] = mine.eval_sample(sample)
             labels[i] = mine.status
         return self._parameters.func_selection(eval_results, labels)
+
+
+class LogisticRegressionModel(Model):
+    _logistic_model: LogisticRegression
+
+    def __init__(self, parameters: Parameters, samples: Iterable[Sample]):
+        super().__init__(self, parameters, samples)
+        data_train = Dataset(samples=samples)
+        labels = data_train.labels
+        attributes = np.concatenate(data_train.attributes)
+        self._logistic_model = LogisticRegression(random_state=0).fit(attributes, labels)
+
+    def classify(self, sample: Sample) -> int:
+        predictions = self._logistic_model.predict(sample.attributes)
+        return 1 if (predictions == 1).sum() / len(predictions) > 0.5 else -1
 
 
 if __name__ == '__main__':
