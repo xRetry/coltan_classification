5ca8cc92f0273e1ad499ac50f173808ca20e9562
xRetry <marcoschwarz.at@gmail.com>
2022-01-02 10:43:15 +0100

    Changed Parameters to dataclass

--- classes/parameters.py
+++ classes/parameters.py
@@ -1,34 +1,22 @@
 from typing import Callable, Optional
+from dataclasses import dataclass, field
+from functions import transformation
+from classes.normalizers import Normalization
 from classes.estimators import Estimator
 
 
+@dataclass
 class Parameters:
     MineClass: object.__class__
-    func_normalize: Callable
-    func_transform: Callable
     func_eval: Callable
-    eval_args: dict
-    func_selection: Callable
-    func_loss: Callable
-    estimator: type(Estimator)
-    mine_kwargs: dict
-
-    def __init__(self, MineClass: object.__class__, func_transform: Callable, func_normalize: Callable,
-                 func_eval: Callable, func_selection: Callable, func_loss: Callable,
-                 estimator: type(Estimator), mine_kwargs:Optional[dict]=None, eval_args:Optional[dict]=None):
-        self.MineClass = MineClass
-        self.func_normalize = func_normalize
-        self.func_transform = func_transform
-        self.func_eval = func_eval
-        self.func_selection = func_selection
-        self.func_loss = func_loss
-        self.estimator = estimator
-        if mine_kwargs is None:
-            mine_kwargs = {}
-        self.mine_kwargs = mine_kwargs
-        if eval_args is None:
-            eval_args = {}
-        self.eval_args = eval_args
+    ModelClass: object.__class__ = None
+    func_normalize: Callable = Normalization.none
+    func_transform: Callable = transformation.none
+    func_selection: Callable = None
+    func_loss: Callable = None
+    estimator: type(Estimator) = None
+    eval_kwargs: dict = field(default_factory=dict)
+    mine_kwargs: dict = field(default_factory=dict)
 
 
 if __name__ == '__main__':
