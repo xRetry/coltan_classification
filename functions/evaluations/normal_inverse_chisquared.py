24841e222bec7ffd07b688cf4ea0155725ff985d
xRetry <marcoschwarz.at@gmail.com>
2022-01-11 12:53:19 +0100

    Added aggregation function to arguments

--- functions/evaluations/normal_inverse_chisquared.py
+++ functions/evaluations/normal_inverse_chisquared.py
@@ -2,6 +2,7 @@ import numpy as np
 import scipy.special
 import scipy.stats
 from functions.utils import verification
+from typing import Callable
 
 
 @verification('a', 'a', '', '', 'ba')
@@ -23,7 +24,7 @@ def posterior(mean: np.ndarray, std: np.ndarray, kappa: int, nu: int, x: np.ndar
 
 
 @verification('a', 'a', '', '', 'a', 'a')
-def pdf(mean, std, kappa: int, nu: int, x_mean: np.ndarray, x_std: np.ndarray) -> float:
+def pdf(mean, std, kappa: int, nu: int, x_mean: np.ndarray, x_std: np.ndarray, func_aggr: Callable=np.product) -> float:
     """
         Evaluates the pdf of a NIX distribution for a given mean and std.
     """
@@ -31,11 +32,11 @@ def pdf(mean, std, kappa: int, nu: int, x_mean: np.ndarray, x_std: np.ndarray) -
     x_var = np.power(x_std, 2)
     Z = np.sqrt(2*np.pi)/np.sqrt(kappa) * scipy.special.gamma(nu/2) * np.power(2 / (nu * var), nu / 2)
     p = 1 / Z * np.power(x_std, -1) * np.power(x_var, -(nu / 2 + 1)) * np.exp(-1 / (2 * x_var) * (nu * var + kappa * np.power(mean - x_mean, 2)))  # TODO: Fix underflow
-    return np.product(p)
+    return func_aggr(p)
 
 
 @verification('a', 'a', '', '', 'a')
-def pdf_predictive(mean: np.ndarray, std: np.ndarray, kappa: int, nu: int, x: np.ndarray) -> float:
+def pdf_predictive(mean: np.ndarray, std: np.ndarray, kappa: int, nu: int, x: np.ndarray, func_aggr: Callable=np.product) -> float:
     """
         Evaluates the predictive pdf of a NIX distribution for new data.
     """
@@ -45,7 +46,7 @@ def pdf_predictive(mean: np.ndarray, std: np.ndarray, kappa: int, nu: int, x: np
         loc=mean,
         scale=(1+kappa)*np.power(std, 2) / kappa
     )
-    return np.product(p)
+    return func_aggr(p)
 
 
 if __name__ == '__main__':
